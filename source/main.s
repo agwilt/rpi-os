89ac2876ca73daf97069818aeb88ef5302cb62f7
Andreas Gwilt <andreas@gwilt.net>
2015-10-26 20:20:14 +0100

    Set up GPIO pin 47

--- source/main.s
+++ source/main.s
@@ -1,6 +1,23 @@
+.set LED_PIN, 47
+.set GPIO_BASE, 0x20200000
+
 .section .init
 .globl _start
 _start:
 
 // sore GPIO controller address in r0
-ldr r0,=0x20200000
+ldr r0, =GPIO_BASE
+
+// next, enable output for pin 47 (001 is output, 000 input)
+mov r1, #1
+lsl r1, #21
+// 4th set of GPIO words (GPIO_BASE+16 bytes)
+str r1, [r0, #16]
+
+// get number to set pin 47
+mov r1, #1
+lsl r1, #15
+
+//loop for ever
+loop$:
+	b loop$
