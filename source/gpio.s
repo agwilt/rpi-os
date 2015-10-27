1a36a19d9978d7e739a569c13be2c1e05963eced
Andreas Gwilt <andreas@gwilt.net>
2015-10-27 13:13:27 +0100

    Move function to separate file

---
+++ source/gpio.s
@@ -0,0 +1,6 @@
+// set mode for a GPIO pin. takes pin number (e.g. 47), mode (GPIO_IN/OUT)
+.globl gpio_setmode
+gpio_setmode:
+	// r0: pin number
+	// r1: mode
+	mov pc, lr
--- source/main.s
+++ source/main.s
@@ -58,10 +58,3 @@ loop$:
 		bne wait2$
 
 	b loop$
-
-// set mode for a GPIO pin. takes pin number (e.g. 47), mode (GPIO_IN/OUT)
-.globl gpio_setmode
-gpio_setmode:
-	// r0: pin number
-	// r1: mode
-	mov pc, lr
