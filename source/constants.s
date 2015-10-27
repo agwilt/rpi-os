7637d4550299542f684111f4a7efaacdbe3f9e9e
Andreas Gwilt <andreas@gwilt.net>
2015-10-27 16:44:20 +0100

    More constants

--- source/constants.s
+++ source/constants.s
@@ -1,5 +1,11 @@
 .set LED_OK, 47
-.set GPIO_BASE, 0x20200000
+
+.set GPSET0, 0x2020001c			// first pin setting word
+.set GPCLR0, 0x20200028			// first pin clearing word
+
+.set GPIO_BASE, 0x20200000		// start of the GPIO registers
+.set GPIO_HIGH, 1
 .set GPIO_IN, 0
+.set GPIO_LOW, 0
 .set GPIO_MAX_PIN, 53
 .set GPIO_OUT, 1
