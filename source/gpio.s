9892674193f3b8ce44737b6fd3c4f1bc097a5ba8
Andreas Gwilt <andreas@gwilt.net>
2015-10-27 15:13:26 +0100

    Fix gpio_set_function to be non-destructive

--- source/gpio.s
+++ source/gpio.s
@@ -30,9 +30,21 @@ gpio_set_function:
 	// r2: address of relevant word (0..5)
 	// r1: function number
 	// now change the word
-	add r0, r0, lsl #1		// multiply r0 by 3
-	lsl r1, r0				// shift code left to correct position
-	str r1, [r2]
+	add r0, r0, lsl #1		// multiply r0 by 3 to get offset
+	mov r3, #0b111			// create mask to bic word
+	// shift mask and function
+	lsl r3, r0
+	lsl r1, r0
+
+	ldr r0, [r2]			// load relevant word into r0
+	bic r0, r0, r3			// clear relevant 3 bits of word
+	// r0: BICed value of relevant word
+	// r1: function number (000..111)
+	// r2: address of relevant word
+	// r3: mask
+	add r0, r1
+	// now write the edited word back
+	str r0, [r2]
 
 
 	//return
