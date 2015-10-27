a0eaa76134cf77801f16f174db2615cd747872af
Andreas Gwilt <andreas@gwilt.net>
2015-10-27 19:50:59 +0100

    Make lights cycle instead of blink

--- source/main.s
+++ source/main.s
@@ -9,26 +9,52 @@ mov r0, #LED_OK				// set pin number
 mov r1, #GPIO_OUT			// set pin function
 bl gpio_set_function
 
+//enable output for the PWR LED pin
+mov r0, #LED_PWR			// set pin number
+mov r1, #GPIO_OUT			// set pin function
+bl gpio_set_function
+
 //loop for ever
 loop$:
 
-	/* switch on */
+	/* both off */
 
-	mov r0, #LED_OK
-	mov r1, #GPIO_HIGH
+	mov r0, #LED_PWR
+	mov r1, #GPIO_LOW
 	bl gpio_output
-
-	mov r0,#0x3F0000
-	bl sleep
-
-
-	/* switch off */
 
 	mov r0, #LED_OK
 	mov r1, #GPIO_LOW
 	bl gpio_output
 
-	mov r0,#0x3F0000
+	mov r0,#0x300000
+	bl sleep
+
+	/* OK on */
+
+	mov r0, #LED_OK
+	mov r1, #GPIO_HIGH
+	bl gpio_output
+
+	mov r0,#0x200000
+	bl sleep
+
+	/* both on */
+
+	mov r0, #LED_PWR
+	mov r1, #GPIO_HIGH
+	bl gpio_output
+
+	mov r0,#0x300000
+	bl sleep
+
+	/* PWR on */
+
+	mov r0, #LED_OK
+	mov r1, #GPIO_LOW
+	bl gpio_output
+
+	mov r0,#0x200000
 	bl sleep
 
 	b loop$
