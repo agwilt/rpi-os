3d98416daaae00410af392b0b6742c6beada713b
Andreas Gwilt <andreas@gwilt.net>
2015-10-27 23:30:59 +0100

    New constants (for the System Timer)

--- source/constants.s
+++ source/constants.s
@@ -1,12 +1,21 @@
-.set LED_OK, 47
-.set LED_PWR, 35
-
-.set GPSET0, 0x2020001c			// first pin setting word
 .set GPCLR0, 0x20200028			// first pin clearing word
-
 .set GPIO_BASE, 0x20200000		// start of the GPIO registers
 .set GPIO_HIGH, 1
 .set GPIO_IN, 0
 .set GPIO_LOW, 0
 .set GPIO_MAX_PIN, 53
 .set GPIO_OUT, 1
+.set GPSET0, 0x2020001c			// first pin setting word
+
+.set LED_OK, 47
+.set LED_PWR, 35
+
+.set ST_BASE, 0x20003000		// base address for the system timer
+.set ST_CS, ST_BASE				// system timer control/status
+.set ST_CL, 0x20003004			// counter
+.set ST_CL0, ST_CL				// counter lower 32 bits
+.set ST_CL1, 0x20003008			// counter higher 32 bits
+.set ST_C0, 0x2000300c			// 0th comparison register
+.set ST_C1, 0x20003010
+.set ST_C2, 0x20003014
+.set ST_C3, 0x20003018
