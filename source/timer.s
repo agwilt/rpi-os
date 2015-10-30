6f4997e6e3e365679c6261b9ee216ce7566a33ad
Andreas Gwilt <andreas@gwilt.net>
2015-10-30 20:37:33 +0100

    Write own sleep that doesn't use st_get_timestamp

--- source/timer.s
+++ source/timer.s
@@ -4,36 +4,16 @@
 sleep:
 	// r0: number of μSeconds to sleep
 
-	/*
-	// I'm making a quick "old" version just to have something here. The rest is for tomorrow.
-	sleep_wait$:
-		sub r0, #1
-		cmp r0, #0
-		bne sleep_wait$
-	*/
-
-	/*
-	// It's now after tomorrow, but let's do this!
-	ldr r1, =ST_CL0				// timestamp at start
-	add r1, r0					// timestamp at end
+	ldr r2, =ST_CL
+	ldr r2, [r2]				// timestamp at start
 	sleep_loop$:
-		ldr r2, =ST_CL0			// current timestamp
-		cmp r2, r1
-		blt sleep_loop$			// if current is lower than end, loop
-	*/
+		ldr r1, =ST_CL
+		ldr r1, [r1]			// current timestamp
+		sub r1, r1, r2			// r1 = current - start = time elapsed
+		cmp r1, r0
+		bls sleep_loop$			// if current is lower than end, loop
 
-	push {lr}
-	mov r2, r0					// delay
-	bl st_get_timestamp
-	mov r3, r0					// start timestamp
-
-	sleep_loop$:
-		bl st_get_timestamp		// current timestamp in r0+r1
-		sub r1, r0, r3
-		cmp r1, r2
-		bls sleep_loop$
-
-	pop {pc}
+	mov pc, lr
 
 
 .globl st_get_timestamp
