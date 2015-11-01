44622224afc7fdaeaeeed9e1b5fe0842da6a54d1
Andreas Gwilt <andreas@gwilt.net>
2015-11-01 13:28:54 +0100

    Start working on proper morse stuff

--- source/main.s
+++ source/main.s
@@ -26,123 +26,59 @@ mov r1, #GPIO_HIGH
 bl gpio_output
 
 // now for the morse stuff
-ldr r4, =200000				// duration
+ldr r6, =200000				// base duration
 add r5, r4, r4, lsl #1		// r5 = 3*r4
 
 loop$:
+	// s
+	ldr r0, =s
+	bl morse
 
-	//S
-	mov r0, #LED_ACT
-	mov r1, r4
-	bl gpio_blink
-	mov r0, r4
-	bl st_sleep
 
-	mov r0, #LED_ACT
-	mov r1, r4
-	bl gpio_blink
-	mov r0, r4
-	bl st_sleep
 
-	mov r0, #LED_ACT
-	mov r1, r4
-	bl gpio_blink
+morse:
+	// r0: pattern
+	// r1: base duration
+	push {lr}
+	push {r4}
+	push {r5}
+	push {r6}
+	push {r7}
+	mov r4, r0				// pattern
+	mov r5, #0				// counter
+	mov r6, #0b11			// mask
+	mov r7, r1				// base duration
 
-	mov r0, r5
-	bl st_sleep
+	morseloop$:
+		mov r0, r4				// pattern in r0
+		add r1, r5, r5			// r2 = 2*counter
+		lsr r0, r1				// shift pattern to the right so important pair is aat right
+		and r0, r6				// clear the rest of the bits
+		// r0 now contains the 'character' code
+		cmp r0, #0				// return if code is 00
+		b morse_end$
 
-	//O
-	mov r0, #LED_ACT
-	mov r1, r5
-	bl gpio_blink
-	mov r0, r4
-	bl st_sleep
+		mul r1, r7, r0			// blink duration is code*base duration
+		mov r0, #LED_OK
+		bl gpio_blink
 
-	mov r0, #LED_ACT
-	mov r1, r5
-	bl gpio_blink
-	mov r0, r4
-	bl st_sleep
+		//loop end stuff
+		add r5, #1
 
-	mov r0, #LED_ACT
-	mov r1, r5
-	bl gpio_blink
+	morse_end$:
+		pop {r7}
+		pop {r6}
+		pop {r5}
+		pop {r4}
+		pop {pc}
 
-	mov r0, r5
-	bl st_sleep
 
-	//S
-	mov r0, #LED_ACT
-	mov r1, r4
-	bl gpio_blink
-	mov r0, r4
-	bl st_sleep
-
-	mov r0, #LED_ACT
-	mov r1, r4
-	bl gpio_blink
-	mov r0, r4
-	bl st_sleep
-
-	mov r0, #LED_ACT
-	mov r1, r4
-	bl gpio_blink
-
-	mov r0, r5
-	bl st_sleep
-
-	b loop$
-
-	
-/*
-//loop for ever
-loop$:
-
-	// both off
-
-	mov r0, #LED_PWR
-	mov r1, #GPIO_LOW
-	bl gpio_output
-
-	mov r0, #LED_OK
-	mov r1, #GPIO_LOW
-	bl gpio_output
-
-	ldr r0,=200000
-	bl st_sleep
-
-	// OK on
-
-	mov r0, #LED_OK
-	mov r1, #GPIO_HIGH
-	bl gpio_output
-
-	ldr r0,=200000
-	bl st_sleep
-
-	// both on
-
-	mov r0, #LED_PWR
-	mov r1, #GPIO_HIGH
-	bl gpio_output
-
-	ldr r0,=200000
-	bl st_sleep
-
-	// PWR on
-
-	mov r0, #LED_OK
-	mov r1, #GPIO_LOW
-	bl gpio_output
-
-	ldr r0,=200000
-	bl st_sleep
-
-	b loop$
-*/
 
 .section .data
 
 .align 2
-pattern:
-	.int 0b11111111101010100010001000101010
+s:
+	.int 0b00000000000000000000000000010101
+.align 2
+o:
+	.int 0b00000000000000000000000000111111
